7d7db3ea8565433ecb117d5116ebdc201b16c069
José Ignacio Tapia <30639989+crashToxic@users.noreply.github.com>
2019-12-01 21:36:55 -0300

    Se configuró el comportamiento de la pelota cuando rebota

--- Pong1.py
+++ Pong1.py
@@ -89,6 +89,10 @@ while True:
         ball.dx *= -1
 
     # Paddle and ball collisions
-    if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
         ball.dx *= -1
