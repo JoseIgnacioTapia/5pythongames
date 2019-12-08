3908c9176a2c899a40c8e6f5761c4c6ba712d6d5
José Ignacio Tapia <30639989+crashToxic@users.noreply.github.com>
2019-12-08 20:39:08 -0300

    Se agregaron: el sistema de puntaje y los efectos de sonido cuando la bola toca las paleta y los bordes de arriba y abajo.

--- Pong1.py
+++ Pong1.py
@@ -1,11 +1,16 @@
 # ATARI GAME
 import turtle
+import winsound
 
 wn = turtle.Screen()
 wn.title("Pong by @TokyoEdTech")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -34,6 +39,14 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 1
 ball.dy = -1
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  PlayerB: 0", align="center", font=("Courier", 24, "normal"))
 
 # Function
 def paddle_a_up():
@@ -75,24 +88,38 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("sound4.mp3", winsound.SND_ASYNC)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("sound4.mp3", winsound.SND_ASYNC)
+
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  PlayerB: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  PlayerB: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("sound4.mp3", winsound.SND_ASYNC)
+
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("sound4.mp3", winsound.SND_ASYNC)
